678e055c1ade5f712faf59cc6983f900f8d7e7b9
iamchandra1511 <68295343+iamchandra1511@users.noreply.github.com>
2020-07-18 03:17:21 +0530

    Add files via upload

---
+++ ThreadCreation.java
@@ -0,0 +1,35 @@
+
+public class ThreadCreation implements Runnable
+{
+	public void run()   
+	{
+		
+		MyMethod();
+	}
+	
+	public synchronized void MyMethod()    //synchronized method
+	{
+		for(int i=1;i<=10;i++)
+		{
+			System.out.println(i);
+			try
+			{
+				Thread.sleep(100);  //sleep method
+			}
+			catch(Exception ex)
+			{
+				
+			}
+		  }
+
+	}
+	public static void main(String args[])
+	{
+		ThreadCreation mt=new ThreadCreation();
+		Thread t1=new Thread(mt);
+		Thread t2=new Thread(mt);
+		t1.start();  //start method
+		t2.start();
+	}
+
+}
