29efc1a8a2933e057a406c3898f318bca300fb13
iamchandra1511 <68295343+iamchandra1511@users.noreply.github.com>
2020-07-18 00:17:21 +0530

    Delete ForLoopExample.java

--- ForLoopExample.java
+++
@@ -1,7 +0,0 @@
-public class ForLoopExample {
-    public static void main(String args[]) {
-        // here, n is declared inside of the for loop
-        for (int n = 10; n > 0; n--)
-            System.out.println("tick " + n);
-    }
-}
