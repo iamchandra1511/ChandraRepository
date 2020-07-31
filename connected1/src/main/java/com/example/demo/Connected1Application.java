47c39adcdd2f6a085e5f4b258355dfac647d7046
unknown <chandra1511amensys@gmail.com>
2020-08-01 02:42:50 +0530

    New File

---
+++ connected1/src/main/java/com/example/demo/Connected1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Connected1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Connected1Application.class, args);
+	}
+
+}
